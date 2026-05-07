5d0f8b7488e3b995f90871328e0a8b334da0384a
malekalhakem <amalk9489@gmail.com>
2026-05-08 01:49:52 +0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import TchouameniVote from "./TchouameniVote"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <TchouameniVote />
+)
